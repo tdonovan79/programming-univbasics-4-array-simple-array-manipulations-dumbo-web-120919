08013b01891c1733139c3a5ddca3c5138ce5daa9
Thomas Donovan <tdonovan79@gmail.com>
2019-10-31 01:02:07 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,52 @@
+def using_push(array, string)
+  return array.push(string)
+end
+
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array, num = 2)
+  num.times do
+    array.pop
+  end
+  return array
+end
+
+def using_shift(array)
+  return array.shift
+end
+
+def shift_with_args(array, num = 2)
+  num.times do
+    array.shift
+  end
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element, 4)
+end
+
+def using_iniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  return array.delte(integer)
+end
